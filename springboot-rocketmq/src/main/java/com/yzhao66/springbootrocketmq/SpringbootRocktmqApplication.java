6ece79ef00e583952803d7d19b2c7ac3fef4b564
yzhao66 <cttclouds@163.com>
2021-05-10 23:00:39 +0800

    整合rockermq

--- springboot-rocketmq/src/main/java/com/yzhao66/springbootrocketmq/SpringbootRocktmqApplication.java
+++ springboot-rocketmq/src/main/java/com/yzhao66/springbootrocketmq/SpringbootRocktmqApplication.java
@@ -1,13 +1,18 @@
 package com.yzhao66.springbootrocketmq;
 
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.Resource;
 
 @SpringBootApplication
 public class SpringbootRocktmqApplication {
 
+	@Resource
+	private RocketMQTemplate rocketMQTemplate;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootRocktmqApplication.class, args);
 	}
-
+	
 }
